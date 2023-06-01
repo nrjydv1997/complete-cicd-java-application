269234403036cda2594fb330a1c6c2863c293801
Nrjydv <nrjydv1997@gmail.com>
2023-06-01 12:41:14 +0530

    cluster code added

---
+++ eks-cluster/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA22WMLNIXL4X7E7VW"
+  secret_key = "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"
+}
---
+++ eks-cluster/worker.tf
@@ -0,0 +1,63 @@
+#-------------------------
+# WORKER NODE GROUP 
+#-------------------------
+
+resource "aws_eks_node_group" "eksnode" {
+  cluster_name    = "eksclusterdemo"
+  node_group_name = "example"
+  node_role_arn   = aws_iam_role.eksnoderole.arn
+  subnet_ids      = ["subnet-0494029d4e9765eff","subnet-0f013d1175068d39d","subnet-00470c30e83a3b8aa"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_eks_cluster.eksdemo,
+    aws_iam_role_policy_attachment.eksnoderole-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eksnoderole-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eksnoderole-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+
+#-------------------------
+# IAM ROLE FOR WORKER NODE GROUP 
+#-------------------------
+
+
+resource "aws_iam_role" "eksnoderole" {
+  name = "eksnoderole"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eksnoderole-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eksnoderole.name
+}
+
+resource "aws_iam_role_policy_attachment" "eksnoderole-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eksnoderole.name
+}
+
+resource "aws_iam_role_policy_attachment" "eksnoderole-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eksnoderole.name
+}
